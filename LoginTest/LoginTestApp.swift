2093becc61baa3c208e3267992d80f4c16dd400a
mike <1905185456@qq.com>
2022-11-26 16:48:55 +0800

    Initial Commit

---
+++ LoginTest/LoginTestApp.swift
@@ -0,0 +1,17 @@
+//
+//  LoginTestApp.swift
+//  LoginTest
+//
+//  Created by zp on 2022/11/26.
+//
+
+import SwiftUI
+
+@main
+struct LoginTestApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
